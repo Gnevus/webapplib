cebf07de583547725f39afa35105aabd888efa3e
Gnevus <21177315>
2016-05-19 22:08:10 +0400

    Add file

--- src/ru/simbirsoft/webapp/model/Library.java
+++ src/ru/simbirsoft/webapp/model/Library.java
@@ -4,5 +4,15 @@ package ru.simbirsoft.webapp.model;
  * Created by Admin on 17.05.16.
  */
 public class Library {
-    String fullName;
+    private final String Name;
+    private final String Autor;
+    private final String Year;
+    private final String Page;
+
+    public Library(String name, String autor, String year, String page) {
+        Name = name;
+        Autor = autor;
+        Year = year;
+        Page = page;
+    }
 }
